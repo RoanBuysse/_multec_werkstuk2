0bfb5a3f9378dbc9334a9a82943b78adb8843bb2
Roan Buysse <roan.buysse@telenet.be>
2018-08-19 14:13:40 +0200

    first commit

---
+++ Werkstuk_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Werkstuk_2
+//
+//  Created by Roan Buysse on 19/08/2018.
+//  Copyright © 2018 Roan Buysse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
